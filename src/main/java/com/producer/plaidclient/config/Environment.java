84ab834981afff0a30b89b1276c4fe2cbf19a343
uncutgems <41138529+uncutgems@users.noreply.github.com>
2024-01-26 22:24:24 -0500

    Remove Plaid secret info

--- src/main/java/com/producer/plaidclient/config/Environment.java
+++ src/main/java/com/producer/plaidclient/config/Environment.java
@@ -9,8 +9,8 @@ import java.util.Map;
 
 public class Environment {
     public static final String sandboxEnv = "https://sandbox.plaid.com";
-    public static final String sandboxClientId = "6398a26c66a3830013af8bc9";
-    public static final String sandboxSecret = "022fa09498fdc9057b118c69c19498";
+    public static final String sandboxClientId = "";
+    public static final String sandboxSecret = "";
     public static final String localDevelopment = "http://localhost:8080";
 
     public static Map<String, Object> kafkaConfigs() {
